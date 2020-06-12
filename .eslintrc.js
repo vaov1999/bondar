bbbe66877121dc463ca2dddd417bb99e663e7165
vaov1999 <vaov1999@gmail.com>
2020-06-12 14:59:30 +0300

    enviroment created@

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,15 @@ module.exports = {
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: false
+      }
+    ],
+    'vue/no-unused-vars': 'warn',
   }
 };
---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>Installed CLI Plugins</h3>
+    <h3>Essential Links</h3>
+    <h3>Ecosystem</h3>
+    <input title="loladad adwd a"
+           type="number" value="1123123123123" />
+  </div>
+</template>
+
+<script>
+
+  const lol = () => 1 + 1;
+export default {
+  name: "HelloWorld",
+  props:
+          {
+            msg: String }
+};
+</script>
+
+<style scoped lang="scss"></style>
